14e6aec2e48dbbba88132d929f0bf2364ebc495f
Sergii Ostapchuk <sergiiostapchuk1@gmail.com>
2024-12-27 17:02:16 -0600

    end of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -16,12 +16,14 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(x =>
+    x.AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200")
+);
 
 app.UseAuthentication();
 app.UseAuthorization();
-
-
 
 app.MapControllers();
 
---
+++ client/src/app/_services/accounts.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Inject, Injectable, signal } from '@angular/core';
+import { User } from '../models/user';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountsService {
+  private http = inject(HttpClient);
+  baseUrl = 'http://localhost:5000/api/';
+  currentUser = signal<User | null>(null);
+
+  login(model: any) {
+    return this.http
+      .post<User>(this.baseUrl + 'account/login', model).pipe(
+        map(user => {
+        if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    )
+  }
+
+  register(model: any) {
+    return this.http
+      .post<User>(this.baseUrl + 'account/register', model).pipe(
+        map(user => {
+        if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    )
+  }
+
+  logOut(){
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+
+  
+}
